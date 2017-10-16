240f64889192f76443b6258f0ddb9a21d0b1b3ac
Shunchao Wang <wang.2799@osu.edu>
2017-10-16 11:01:12 -0400

    Added dependency from app to core.

--- code/app/src/main/groovy/me/smartstart/app/AppApplication.groovy
+++ code/app/src/main/groovy/me/smartstart/app/AppApplication.groovy
@@ -1,9 +1,12 @@
 package me.smartstart.app
 
+import me.smartstart.core.CoreApplication
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(CoreApplication.class)
 class AppApplication {
 
 	static void main(String[] args) {
