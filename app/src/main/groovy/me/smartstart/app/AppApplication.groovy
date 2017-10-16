798523f5b62d792189337cee17714f82faa754b9
Shunchao Wang <wang.2799@osu.edu>
2017-10-16 10:13:10 -0400

    Initial import.

---
+++ app/src/main/groovy/me/smartstart/app/AppApplication.groovy
@@ -0,0 +1,12 @@
+package me.smartstart.app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run AppApplication, args
+	}
+}
---
+++ core/src/main/groovy/me/smartstart/core/CoreApplication.groovy
@@ -0,0 +1,12 @@
+package me.smartstart.core
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CoreApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CoreApplication, args
+	}
+}
