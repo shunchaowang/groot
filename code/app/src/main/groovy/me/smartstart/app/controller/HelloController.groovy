d1581fc7fc35f84b66b9a23c27c0fa59bb893d44
Shunchao Wang <wang.2799@osu.edu>
2017-10-17 12:41:01 -0400

    Updated h2 sql files.

--- code/app/src/main/groovy/me/smartstart/app/controller/HelloController.groovy
+++ code/app/src/main/groovy/me/smartstart/app/controller/HelloController.groovy
@@ -1,13 +1,19 @@
 package me.smartstart.app.controller
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
-class HelloWorld {
+@Controller
+class HelloController {
 
     @RequestMapping('/')
     String home() {
-        '*** Hello World ***'
+        '*** Hello Home ***'
+    }
+
+    @RequestMapping('/hello')
+    String hello() {
+        return 'hello/hello'
     }
 }
