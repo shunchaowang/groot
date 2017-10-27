0e8d8a6029bb89ee0abfdb62b7c9f314bf61dbf3
Shunchao Wang <shunchao.wang@gmail.com>
2017-10-26 22:10:59 -0400

    Select.

--- code/app/src/main/resources/static/js/user/index.js
+++ code/app/src/main/resources/static/js/user/index.js
@@ -32,39 +32,43 @@
         var userTable = $('#user-table').DataTable({
             dom: 'Bfrtip',
             buttons: [
+                'create',
                 'colvis',
                 'excel',
-                'print',
-                'create'
+                'print'
             ],
-            'processing': true,
-            'serverSide': true,
-            'paging': true,
-            "paginationType": "full_numbers",
-            "order": [[0, "desc"]],
-            'ajax': {
+            select: true,
+
+            processing: true,
+            serverSide: true,
+            paging: true,
+            paginationType: 'full_numbers',
+            order: [[0, "desc"]],
+            ajax: {
                 'url': listUserUrl,
-                'type': "GET",
+                'type': "get",
                 'dataType': 'json'
             },
 
             // MUST HAVE DATA ON COLUMNDEFS IF SERVER RESPONSE IS JSON ARRAY!!!
-            'columnDefs': [
-                {'name': 'id', 'targets': 0, 'visible': false, 'data': 'id'},
-                {
-                    'name': 'username', 'targets': 1, 'data': 'username', 'orderable': false
-                },
-                {
-                    'name': 'firstName', 'targets': 2, 'data': 'firstName', 'searchable': true,
-                    'orderable': false
-                },
-                {
-                    'name': 'lastName', 'targets': 3, 'data': 'lastName', 'searchable': true,
-                    'orderable': false
-                },
-                {'name': 'dateCreated', 'targets': 4, 'searchable': false, 'data': 'dateCreated'},
-                {'name': 'lastUpdated', 'targets': 5, 'searchable': false, 'data': 'lastUpdated'}
+            columnDefs: [
+                {name: 'id', targets: 0, visible: false, data: 'id'},
+                {name: 'username', targets: 1, data: 'username', orderable: false},
+                {name: 'firstName', targets: 2, data: 'firstName', searchable: true, orderable: false},
+                {name: 'lastName', targets: 3, data: 'lastName', searchable: true, orderable: false},
+                {name: 'dateCreated', targets: 4, searchable: false, data: 'dateCreated'},
+                {name: 'lastUpdated', targets: 5, searchable: false, data: 'lastUpdated'}
             ]
         });
+
+        userTable
+            .on( 'select', function ( e, dt, type, indexes ) {
+                var rowData = userTable.rows( indexes ).data().toArray();
+                events.prepend( '<div><b>'+type+' selection</b> - '+JSON.stringify( rowData )+'</div>' );
+            } )
+            .on( 'deselect', function ( e, dt, type, indexes ) {
+                var rowData = userTable.rows( indexes ).data().toArray();
+                events.prepend( '<div><b>'+type+' <i>de</i>selection</b> - '+JSON.stringify( rowData )+'</div>' );
+            } );
     });
 })(jQuery);
